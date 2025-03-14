4071b4959b2469ad7b114ecba1514fb841ea2ca7
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2025-03-13 16:08:49 -1000

    made calibrator into module

---
+++ calibrator/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parsl_scheduling_simulator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["simcal"],  # Add dependencies if needed
+)
