71bcbbcd19b9e4b354a68bd5b9eca945f9fa29db
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2025-03-13 16:30:22 -1000

    Update __init__.py

--- calibrator/parsl_scheduling_simulator/__init__.py
+++ calibrator/parsl_scheduling_simulator/__init__.py
@@ -1,12 +1,7 @@
-import os
-import glob
-
-# Automatically import all Python files in the current directory (except __init__.py)
-module_files = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
-__all__ = []
-
-for file in module_files:
-    module_name = os.path.basename(file)[:-3]  # Remove .py extension
-    if module_name != "__init__":  # Skip __init__.py itself
-        __all__.append(module_name)
-        exec(f"from .{module_name} import *")
+from .calibrator import *
+from .simulation_server import *
+from .simulator import *
+from .metrics import *
+from .loss import *
+from .util import *
+from .alg_manager import *
