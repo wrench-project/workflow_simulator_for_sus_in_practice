fca5b95e535eda338a96e3a79bb4fa3f19b806cb
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2025-05-02 14:04:18 -1000

    more examples

--- calibrator/exampleCalibrationCall.sh
+++ calibrator/exampleCalibrationCall.sh
@@ -1,2 +1,2 @@
-#Call the simulator using a calibration to generate a simulation output
+#Call the simulator using a calibration to generate a loss
 ./simulator.py calibration -p workflow_simulator -t template.json -e ../data/forkjoin.json -tss most_flops -wss fastest_cores -nss one_core -c '{"platform":{"wms":{"disk_read_bandwidth":"100MBps","disk_write_bandwidth":"100MBps","network_bandwidth":"10Gbps"},"workers":{"worker1":{"speed":"1f","network_bandwidth":"10Gbps"},"worker2":{"speed":"1f","network_bandwidth":"10Gbps"}}},"scheduling":{"task_scheduling_overhead":1,"task_ready_delay": 0}}' -v
